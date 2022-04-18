7711b8fc19116b8fad102a46fc8971da03bb12a1
Danil <dan.frais2000@gmail.com>
2022-04-18 18:51:50 +0300

    add inline cancel, functional like history and add delete message

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -9,6 +9,7 @@ def check_client_in_db():
     inline = InlineKeyboardMarkup(row_width=2)
     [inline.insert(InlineKeyboardButton(text=str(element), callback_data=callback_data))
      for element, callback_data in zip(['Да', 'Нет'], ['yes', 'not'])]
+    inline.row(InlineKeyboardButton(text='Отмена', callback_data='cancel'))
     return inline
 
 def chooce_year(reanswear: str=None):
@@ -22,23 +23,26 @@ def chooce_year(reanswear: str=None):
         [inline.insert(
             InlineKeyboardButton(text=str(year), callback_data=str(year))
         ) for year in range(2018, int(date.today().year)+4)]
+    inline.row(InlineKeyboardButton(text='Отмена', callback_data='cancel'))
     return inline
 
 def choose_month():
     inline = InlineKeyboardMarkup(row_width=2)
     for element in ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
-     'Июль', 'Август', 'Сеньтябрь', 'Октябрь', 'Ноябрь', 'Январь']:
+                    'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']:
         element = InlineKeyboardButton(text=element, callback_data=element)
         inline.insert(element)
+    inline.row(InlineKeyboardButton(text='Отмена', callback_data='cancel'))
     return inline
 
 def choose_day_in_month(month: str):
     inline = InlineKeyboardMarkup(row_width=3)
     day_in_month = {'Январь': 31, 'Февраль': 28, 'Март': 31, 'Апрель': 30, 'Май': 31, 'Июнь': 30,
-     'Июль': 31, 'Август': 31, 'Сентябрь': 30, 'Октябрь': 31, 'Ноябрь': 30, 'Декабрь': 31}
+                    'Июль': 31, 'Август': 31, 'Сентябрь': 30, 'Октябрь': 31, 'Ноябрь': 30, 'Декабрь': 31}
     for element in range(1, day_in_month.get(month)+1):
         element = InlineKeyboardButton(text=str(element), callback_data=str(element))
         inline.insert(element)
+    inline.row(InlineKeyboardButton(text='Отмена', callback_data='cancel'))
     return inline
 
 def choose_time():
@@ -48,6 +52,7 @@ def choose_time():
         inline.insert(keyboard)
         keyboard = InlineKeyboardButton(text=f'{element}:30', callback_data=f'{element}:30')
         inline.insert(keyboard)
+    inline.row(InlineKeyboardButton(text='Отмена', callback_data='cancel'))
     return inline
 
 def choose_new_client_or_not():
@@ -56,28 +61,60 @@ def choose_new_client_or_not():
     inline.insert(element)
     element = InlineKeyboardButton(text='Клиент еще не был записан', callback_data='not')
     inline.insert(element)
+    inline.row(InlineKeyboardButton(text='Отмена', callback_data='cancel'))
     return inline
 
 def choose_alphabet_for_search():
     temp_str = 'абвгдеёжзийклмнопрстуфхцчшщэюя'.upper()
     inline = InlineKeyboardMarkup(row_width=4)
     [inline.insert(InlineKeyboardButton(text=element, callback_data=element)) for element in temp_str]
+    inline.row(InlineKeyboardButton(text='Отмена', callback_data='cancel'))
     return inline
 
-def choose_client_in_db(letter):
+def choose_client_in_db(letter, page):
     inline = InlineKeyboardMarkup(row_width=2)
-    for element in search_client_with_alphabet(letter):
-        inline.insert(InlineKeyboardButton(text=f'{element.last_name} {element.first_name}',
-                                           callback_data=f'{element.last_name} {element.first_name}'))
+    clients = search_client_with_alphabet(letter)
+    if len(clients) < 8:
+        for element in clients:
+            inline.insert(InlineKeyboardButton(text=f'{element.last_name} {element.first_name}',
+                                               callback_data=f'{element.last_name} {element.first_name}'))
+    else:
+        clients = search_client_with_alphabet(letter)[(page*6):(page*6)+6]
+        for element in clients:
+            inline.insert(InlineKeyboardButton(text=f'{element.last_name} {element.first_name}',
+                                               callback_data=f'{element.last_name} {element.first_name}'))
+        inline.insert(InlineKeyboardButton(text='<', callback_data='<'))
+        inline.insert(InlineKeyboardButton(text='>', callback_data='>'))
+    inline.row(InlineKeyboardButton(text='Отмена', callback_data='cancel'))
     return inline
 
 def inline_cancel():
     inline = InlineKeyboardMarkup(row_width=1)
     inline.insert(InlineKeyboardButton(text='Фото не требуется', callback_data='None'))
+    inline.row(InlineKeyboardButton(text='Отмена', callback_data='cancel'))
     return inline
 
 def inline_date_visit(client_id):
-    inline = InlineKeyboardButton(row_width=2)
+    inline = InlineKeyboardMarkup(row_width=4)
     for element in search_history_visit_client(client_id):
         inline.insert(InlineKeyboardButton(text=element.date, callback_data=element.date))
+    inline.row(InlineKeyboardButton(text='Отмена', callback_data='cancel'))
+    return inline
+
+def inline_more_date():
+    inline = InlineKeyboardMarkup(row_width=2)
+    inline.insert(InlineKeyboardButton(text='Просмотреть другие даты', callback_data='None'))
+    inline.insert(InlineKeyboardButton(text='Закончить просмотр', callback_data='Done'))
+    inline.row(InlineKeyboardButton(text='Отмена', callback_data='cancel'))
+    return inline
+
+def inline_cancel_done():
+    inline = InlineKeyboardMarkup(row_width=1)
+    inline.insert(InlineKeyboardButton(text='Готово', callback_data='continue'))
+    inline.row(InlineKeyboardButton(text='Отмена', callback_data='cancel'))
+    return inline
+
+def cancel_fsm():
+    inline = InlineKeyboardMarkup(row_width=1)
+    inline.row(InlineKeyboardButton(text='Отмена', callback_data='cancel'))
     return inline
